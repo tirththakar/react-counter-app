9d0cef328c2804716117a676b2b6fc7f982341c8
TirthT <tirth.thakar174@gmail.com>
2022-09-22 16:01:07 +1200

    Making a React.js counter app in which you can increment counters and delete them and also has a navbar counter which keeps track of unique counters

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill bg-secondary">{totalCounters}</span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
