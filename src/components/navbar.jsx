cc75d709b88b93afb00f870c638552c34fc97173
TirthT <tirth.thakar174@gmail.com>
2022-09-22 16:05:39 +1200

    Exploring mounting and rendering states

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 const NavBar = ({ totalCounters }) => {
+  console.log("Navbar - Rendered");
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
